90a617d73f626450907788fc996c53a22e51949e
kki32 <p107.kitisopakul@gmail.com>
2016-02-09 10:26:32 +0700

    test the whole class for chat

--- tests/unitTestPython.py
+++ tests/unitTestPython.py
@@ -59,6 +59,216 @@ class TestChat(unittest.TestCase):
         os.remove(os.path.join(os.getcwd()+"/receiver.txt"))
         if(self.connected):
             microgear.mqtt_client.disconnect()
+
+    def testCode4Case1(self):
+        """chat with itself"""   
+        self.assertIsNone(microgear.gearkey)
+        self.assertIsNone(microgear.gearsecret)    
+        self.assertIsNone(microgear.appid)
+
+        client.create(self.gearkey, self.gearsecret, self.appid)
+        client.setalias(self.gearname)
+        client.on_message = MagicMock()
+        client.on_connect = MagicMock()
+        client.connect()
+        time.sleep(connect_timeout)
+        self.assertTrue(client.on_connect.called)
+        self.connected = True
+        client.chat(self.gearname, self.message)
+        time.sleep(message_timeout)
+        self.assertTrue(client.on_message.called)
+        client.on_message.assert_called_once_with(self.expectedMsgTopic, self.expectedMessage)
+        
+
+          #require helper 31
+    def testCode4Case2(self):
+        """chat with other microgear in same appid"""  
+        print("run helper...")
+        code = str(31)
+        args = ['python3', 'helper.py', code]
+        p = subprocess.Popen(args, cwd=(helper_dir))
+        time.sleep(connect_worst_timeout)
+
+        print("run main...")
+        self.assertIsNone(microgear.gearkey)
+        self.assertIsNone(microgear.gearsecret)    
+        self.assertIsNone(microgear.appid)
+        client.create(self.gearkey, self.gearsecret, self.appid)
+        client.setalias(self.gearname)
+
+        client.on_connect = MagicMock()
+        
+        client.connect()
+        time.sleep(connect_timeout)
+
+        self.assertTrue(client.on_connect.called)
+        self.connected = True
+        client.chat(self.helperGearname, self.message)
+        time.sleep(message_timeout)
+        r_file = open(receiver_file, "r")
+        received_message = r_file.read()
+        r_file.close()        
+        if(received_message == self.expectedMessage):
+            self.received = True
+
+        self.assertTrue(self.received)
+
+        p.kill()
+
+        #helper 11
+    def testCode4Case3(self):
+        """chat with other microgear in different appid"""   
+  
+        print("run helper...")
+        code = str(11)
+        args = ['python3', 'helper.py', code]
+        p = subprocess.Popen(args, cwd=(helper_dir))
+        time.sleep(connect_worst_timeout)
+
+        print("run main...")
+
+        self.assertIsNone(microgear.gearkey)
+        self.assertIsNone(microgear.gearsecret)    
+        self.assertIsNone(microgear.appid)
+
+        client.create(self.gearkey, self.gearsecret, self.appid)
+        client.setalias(self.gearname)
+
+        client.on_connect = MagicMock()
+        
+        client.connect()
+        time.sleep(connect_timeout)
+        self.assertTrue(client.on_connect.called)
+        self.connected = True
+        client.chat(self.helperGearname, self.message)
+        time.sleep(message_timeout)
+        receiver_file = open(os.path.join(os.getcwd(),"receiver.txt"), "r")
+        received_message = receiver_file.read()
+        receiver_file.close()
+        if(received_message == self.expectedMessage):
+            self.received = True
+        self.assertFalse(self.received)
+        p.kill()
+        
+    #helper 33
+    def testCode4Case5(self):
+        """chat to microgear which shares the same name as itself"""   
+        time.sleep(15)
+        print("run helper...")
+        code = str(33)
+        args = ['python3', 'helper.py', code]
+        p = subprocess.Popen(args, cwd=(helper_dir))
+        time.sleep(connect_worst_timeout)
+
+        print("run main...")
+        self.helperGearname = self.gearname
+        # self.assertIsNone(microgear.gearkey)
+        # self.assertIsNone(microgear.gearsecret)    
+        # self.assertIsNone(microgear.appid)
+        client.create(self.gearkey, self.gearsecret, self.appid)
+        client.setalias(self.gearname)
+
+        client.on_connect = MagicMock()
+        client.on_message = MagicMock()
+        client.connect()
+        time.sleep(connect_timeout)
+        self.assertTrue(client.on_connect.called)
+        self.connected = True
+
+        client.chat(self.helperGearname, self.message)
+        time.sleep(message_timeout)
+
+        self.assertTrue(client.on_message.called)
+        client.on_message.assert_called_once_with(self.expectedMsgTopic, self.expectedMessage)
+        
+        r_file = open(receiver_file, "r")
+        received_message = r_file.read()
+        r_file.close()
+        if(received_message == self.expectedMessage):
+            self.received = True
+
+        self.assertTrue(self.received)
+    
+        p.kill()
+
+    #helper 12
+    def testCode4Case6(self):
+        """chat with other microgear which shares the same gearname in different appid"""
+
+        print("run helper...")
+        code = str(12)
+        args = ['python3', 'helper.py', code]
+        p = subprocess.Popen(args, cwd=(helper_dir))
+        time.sleep(connect_worst_timeout)   
+
+        print("run main...")
+        self.helperGearname = self.gearname
+        # self.assertIsNone(microgear.gearkey)
+        # self.assertIsNone(microgear.gearsecret)    
+        # self.assertIsNone(microgear.appid)
+
+        client.create(self.gearkey, self.gearsecret, self.appid)
+        client.setalias(self.gearname)
+
+        client.on_connect = MagicMock()
+        client.on_message = MagicMock()
+        
+        client.connect()
+        time.sleep(connect_timeout)
+        self.assertTrue(client.on_connect.called)
+        self.connected = True
+        client.chat(self.helperGearname, self.message)
+        time.sleep(message_timeout)
+
+        self.assertTrue(client.on_message.called)
+        client.on_message.assert_called_once_with(self.expectedMsgTopic, self.expectedMessage)
+        
+        receiver_file = open(os.path.join(os.getcwd(),"receiver.txt"), "r")
+        received_message = receiver_file.read()
+        receiver_file.close()
+        if(received_message == self.expectedMessage):
+            self.received = True
+            
+        self.assertFalse(self.received)
+        p.kill()
+
+        #helper 32
+    def testCode4Case8(self):
+        """chat to microgear which has gearname similar to topic"""   
+
+        print("run helper...")
+        code = str(32)
+        args = ['python3', 'helper.py', code]
+        p = subprocess.Popen(args, cwd=(helper_dir))
+        time.sleep(connect_worst_timeout)
+
+        print("run main...")
+        self.gearname = '/firstTopic'
+        # self.assertIsNone(microgear.gearkey)
+        # self.assertIsNone(microgear.gearsecret)    
+        # self.assertIsNone(microgear.appid)
+
+        client.create(self.gearkey, self.gearsecret, self.appid)
+        client.setalias(self.gearname)
+
+        client.on_connect = MagicMock()
+        client.connect()
+        time.sleep(connect_timeout)
+        self.assertTrue(client.on_connect.called)
+        self.connected = True
+
+        client.chat(self.helperGearname, self.message)
+        time.sleep(message_timeout)
+
+        receiver_file = open(os.path.join(os.getcwd(),"receiver.txt"), "r")
+        received_message = receiver_file.read()
+        receiver_file.close()
+        if(received_message == self.expectedMessage):
+            self.received = True
+        self.assertFalse(self.received)
+        p.kill()
+
+
     #helper 34
     def testCode4Case9(self):
         """chat with other microgear which has empty string as gearname"""
@@ -97,7 +307,7 @@ class TestChat(unittest.TestCase):
             self.received = True
         self.assertFalse(self.received)
         p.kill()
-    
+
     #helper 61
     def testCode4Case10(self):
         """chat to topic which has subscriber"""   
@@ -133,214 +343,6 @@ class TestChat(unittest.TestCase):
             self.received = True
         self.assertFalse(self.received)
         p.kill()
-    # def testCode4Case1(self):
-    #     """chat with itself"""   
-    #     self.assertIsNone(microgear.gearkey)
-    #     self.assertIsNone(microgear.gearsecret)    
-    #     self.assertIsNone(microgear.appid)
-
-    #     client.create(self.gearkey, self.gearsecret, self.appid)
-    #     client.setalias(self.gearname)
-    #     client.on_message = MagicMock()
-    #     client.on_connect = MagicMock()
-    #     client.connect()
-    #     time.sleep(connect_timeout)
-    #     self.assertTrue(client.on_connect.called)
-    #     self.connected = True
-    #     client.chat(self.gearname, self.message)
-    #     time.sleep(message_timeout)
-    #     self.assertTrue(client.on_message.called)
-    #     client.on_message.assert_called_once_with(self.expectedMsgTopic, self.expectedMessage)
-        
-
-    #       #require helper 31
-    # def testCode4Case2(self):
-    #     """chat with other microgear in same appid"""  
-    #     print("run helper...")
-    #     code = str(31)
-    #     args = ['python3', 'helper.py', code]
-    #     p = subprocess.Popen(args, cwd=(helper_dir))
-    #     time.sleep(connect_worst_timeout)
-
-    #     print("run main...")
-    #     self.assertIsNone(microgear.gearkey)
-    #     self.assertIsNone(microgear.gearsecret)    
-    #     self.assertIsNone(microgear.appid)
-    #     client.create(self.gearkey, self.gearsecret, self.appid)
-    #     client.setalias(self.gearname)
-
-    #     client.on_connect = MagicMock()
-        
-    #     client.connect()
-    #     time.sleep(connect_timeout)
-
-    #     self.assertTrue(client.on_connect.called)
-    #     self.connected = True
-    #     client.chat(self.helperGearname, self.message)
-    #     time.sleep(message_timeout)
-    #     r_file = open(receiver_file, "r")
-    #     received_message = r_file.read()
-    #     r_file.close()        
-    #     if(received_message == self.expectedMessage):
-    #         self.received = True
-
-    #     self.assertTrue(self.received)
-
-    #     p.kill()
-
-    #     #helper 11
-    # def testCode4Case3(self):
-    #     """chat with other microgear in different appid"""   
-  
-    #     print("run helper...")
-    #     code = str(11)
-    #     args = ['python3', 'helper.py', code]
-    #     p = subprocess.Popen(args, cwd=(helper_dir))
-    #     time.sleep(connect_worst_timeout)
-
-    #     print("run main...")
-
-    #     self.assertIsNone(microgear.gearkey)
-    #     self.assertIsNone(microgear.gearsecret)    
-    #     self.assertIsNone(microgear.appid)
-
-    #     client.create(self.gearkey, self.gearsecret, self.appid)
-    #     client.setalias(self.gearname)
-
-    #     client.on_connect = MagicMock()
-        
-    #     client.connect()
-    #     time.sleep(connect_timeout)
-    #     self.assertTrue(client.on_connect.called)
-    #     self.connected = True
-    #     client.chat(self.helperGearname, self.message)
-    #     time.sleep(message_timeout)
-    #     receiver_file = open(os.path.join(os.getcwd(),"receiver.txt"), "r")
-    #     received_message = receiver_file.read()
-    #     receiver_file.close()
-    #     if(received_message == self.expectedMessage):
-    #         self.received = True
-    #     self.assertFalse(self.received)
-    #     p.kill()
-        
-    # #helper 33
-    # def testCode4Case5(self):
-    #     """chat to microgear which shares the same name as itself"""   
-    #     time.sleep(15)
-    #     print("run helper...")
-    #     code = str(33)
-    #     args = ['python3', 'helper.py', code]
-    #     p = subprocess.Popen(args, cwd=(helper_dir))
-    #     time.sleep(connect_worst_timeout)
-
-    #     print("run main...")
-    #     self.helperGearname = self.gearname
-    #     # self.assertIsNone(microgear.gearkey)
-    #     # self.assertIsNone(microgear.gearsecret)    
-    #     # self.assertIsNone(microgear.appid)
-    #     client.create(self.gearkey, self.gearsecret, self.appid)
-    #     client.setalias(self.gearname)
-
-    #     client.on_connect = MagicMock()
-    #     client.on_message = MagicMock()
-    #     client.connect()
-    #     time.sleep(connect_timeout)
-    #     self.assertTrue(client.on_connect.called)
-    #     self.connected = True
-
-    #     client.chat(self.helperGearname, self.message)
-    #     time.sleep(message_timeout)
-
-    #     self.assertTrue(client.on_message.called)
-    #     client.on_message.assert_called_once_with(self.expectedMsgTopic, self.expectedMessage)
-        
-    #     r_file = open(receiver_file, "r")
-    #     received_message = r_file.read()
-    #     r_file.close()
-    #     if(received_message == self.expectedMessage):
-    #         self.received = True
-
-    #     self.assertTrue(self.received)
-    
-    #     p.kill()
-
-    # #helper 12
-    # def testCode4Case6(self):
-    #     """chat with other microgear which shares the same gearname in different appid"""
-
-    #     print("run helper...")
-    #     code = str(12)
-    #     args = ['python3', 'helper.py', code]
-    #     p = subprocess.Popen(args, cwd=(helper_dir))
-    #     time.sleep(connect_worst_timeout)   
-
-    #     print("run main...")
-    #     self.helperGearname = self.gearname
-    #     # self.assertIsNone(microgear.gearkey)
-    #     # self.assertIsNone(microgear.gearsecret)    
-    #     # self.assertIsNone(microgear.appid)
-
-    #     client.create(self.gearkey, self.gearsecret, self.appid)
-    #     client.setalias(self.gearname)
-
-    #     client.on_connect = MagicMock()
-    #     client.on_message = MagicMock()
-        
-    #     client.connect()
-    #     time.sleep(connect_timeout)
-    #     self.assertTrue(client.on_connect.called)
-    #     self.connected = True
-    #     client.chat(self.helperGearname, self.message)
-    #     time.sleep(message_timeout)
-
-    #     self.assertTrue(client.on_message.called)
-    #     client.on_message.assert_called_once_with(self.expectedMsgTopic, self.expectedMessage)
-        
-    #     receiver_file = open(os.path.join(os.getcwd(),"receiver.txt"), "r")
-    #     received_message = receiver_file.read()
-    #     receiver_file.close()
-    #     if(received_message == self.expectedMessage):
-    #         self.received = True
-            
-    #     self.assertFalse(self.received)
-    #     p.kill()
-
-    #     #helper 32
-    # def testCode4Case8(self):
-    #     """chat to microgear which has gearname similar to topic"""   
-
-    #     print("run helper...")
-    #     code = str(32)
-    #     args = ['python3', 'helper.py', code]
-    #     p = subprocess.Popen(args, cwd=(helper_dir))
-    #     time.sleep(connect_worst_timeout)
-
-    #     print("run main...")
-    #     self.gearname = '/firstTopic'
-    #     # self.assertIsNone(microgear.gearkey)
-    #     # self.assertIsNone(microgear.gearsecret)    
-    #     # self.assertIsNone(microgear.appid)
-
-    #     client.create(self.gearkey, self.gearsecret, self.appid)
-    #     client.setalias(self.gearname)
-
-    #     client.on_connect = MagicMock()
-    #     client.connect()
-    #     time.sleep(connect_timeout)
-    #     self.assertTrue(client.on_connect.called)
-    #     self.connected = True
-
-    #     client.chat(self.helperGearname, self.message)
-    #     time.sleep(message_timeout)
-
-    #     receiver_file = open(os.path.join(os.getcwd(),"receiver.txt"), "r")
-    #     received_message = receiver_file.read()
-    #     receiver_file.close()
-    #     if(received_message == self.expectedMessage):
-    #         self.received = True
-    #     self.assertFalse(self.received)
-    #     p.kill()
-
 def main():
     #suite = unittest.TestSuite()
     #suite.addTest(TestChat("testCode4Case2"))
